6f783c6d0fd863e5418bb5bc1ed649fc74ac0791
emanuelhix <parker.e.hix@gmail.com>
2024-11-13 03:24:22 -0600

    Added statements to view all rows of every table

--- print-tables.sql
+++ print-tables.sql
@@ -1,1 +1,38 @@
+-- Display all data from ClientNeeds
+SELECT * FROM ClientNeeds;
+
+-- Display all data from EmergencyContact
+SELECT * FROM EmergencyContact;
+
+-- Display all data from Expense
+SELECT * FROM Expense;
+
+-- Display all data from InsurancePolicy
+SELECT * FROM InsurancePolicy;
+
+-- Display all data from Team
 SELECT * FROM Team;
+
+-- Display all data from Volunteer
+SELECT * FROM Volunteer;
+
+-- Display all data from Client
+SELECT * FROM Client;
+
+-- Display all data from CheckDonation
+SELECT * FROM CheckDonation;
+
+-- Display all data from CardDonation
+SELECT * FROM CardDonation;
+
+-- Display all data from Donation
+SELECT * FROM Donation;
+
+-- Display all data from Donor
+SELECT * FROM Donor;
+
+-- Display all data from Employee
+SELECT * FROM Employee;
+
+-- Display all data from Person
+SELECT * FROM Person;
