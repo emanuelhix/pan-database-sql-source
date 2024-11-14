f5cd969c57ca92d64a5df5354bd29c1107291095
emanuelhix <parker.e.hix@gmail.com>
2024-11-14 02:23:49 -0600

    Added more procedures to file, changed format

--- nuke-procedures.sql
+++ nuke-procedures.sql
@@ -1,3 +1,33 @@
-DROP PROCEDURE IF EXISTS InsertTeam;
-DROP PROCEDURE IF EXISTS InsertPerson;
+-- Drop procedures if they exist
 
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertTeam')
+    DROP PROCEDURE InsertTeam;
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertPerson')
+    DROP PROCEDURE InsertPerson;
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertClient')
+    DROP PROCEDURE InsertClient;
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertCares')
+    DROP PROCEDURE InsertCares;
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertClientNeeds')
+    DROP PROCEDURE InsertClientNeeds;
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertInsures')
+    DROP PROCEDURE InsertInsures;
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertVolunteer')
+    DROP PROCEDURE InsertVolunteer;
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertServes')
+    DROP PROCEDURE InsertServes;
+GO
